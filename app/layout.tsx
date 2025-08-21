13bd12c916d797117bce07db8cb8d30ad7dcbbd3
cloudzqy <cloudzqy93@gmail.com>
2025-08-21 11:25:26 +0800

    图片

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 
 const title = '周青云❤️田柳的婚礼邀请';
 const description = '2025.10.3南县滨江大酒店富贵厅，诚挚邀请您参加我们的婚礼';
-const image = 'https://marry-app-ruddy.vercel.app/assets/logo.png';
+const image = '/assets/logo.png';
 
 export const metadata = {
   title,
